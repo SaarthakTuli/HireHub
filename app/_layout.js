fa80b65c2f1b545100f20660016b23fe9c388397
SaarthakTuli <saarthaktuli@gmail.com>
2023-04-09 16:45:02 +0530

    welcome

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,30 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(
+    async (req, res) => {
+      // Show Home page only if fonts have loaded...
+      if (fontsLoaded) {
+        await SplashScreen.hideAsync();
+      }
+    },
+    [fontsLoaded]
+  );
+
+  if (!fontsLoaded) return null;
+
+  return <Stack onLayout={onLayoutRootView} />;
 };
 
 export default Layout;
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,75 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
 
-import styles from './welcome.style'
+import { useRouter } from "expo-router";
+
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
+import { set } from "react-native-reanimated";
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
+  const router = useRouter();
+
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      {/* TOP HEADER */}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
 
-export default Welcome
+      {/* SEARCH BAR */}
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="What are you looking for?"
+            value=""
+            onChangeText={() => {}}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
